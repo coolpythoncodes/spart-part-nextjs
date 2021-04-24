979c4f1303ddc6c1fabbf2effd9627b8c09b07b5
coolpythoncodes <coolpythoncodes@gmail.com>
2021-04-24 21:48:56 +0100

    helping hands section

---
+++ components/HelpingHands.js
@@ -0,0 +1,23 @@
+const HelpingHands = () => {
+    return (
+        <div className="text-center sm:flex sm:space-x-10">
+            <div>
+                <img className="mx-auto" src="/icon.png" alt=""/>
+                <h3 className="text-sm font-bold text-center">Helping Hands</h3>
+                <p className="text-sm text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, exercitationem! Eos!</p>
+            </div>
+            <div className="my-5 sm:my-0">
+                <img className="mx-auto" src="/icon.png" alt=""/>
+                <h3 className="text-sm font-bold text-center">Helping Hands</h3>
+                <p className="text-sm text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, exercitationem! Eos!</p>
+            </div>
+            <div>
+                <img className="mx-auto" src="/icon.png" alt=""/>
+                <h3 className="text-sm font-bold text-center">Helping Hands</h3>
+                <p className="text-sm text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, exercitationem! Eos!</p>
+            </div>
+        </div>
+    )
+}
+
+export default HelpingHands
